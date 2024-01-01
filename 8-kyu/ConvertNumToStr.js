ad638b908fbb605f23676e27cf70944e1eb390b9
Gurnav224 <chaudharyg856@gmail.com>
2024-01-01 14:55:04 +0530

    codwars solve number to string

---
+++ 8-kyu/ConvertNumToStr.js
@@ -0,0 +1,10 @@
+
+function numberToString(num) {
+    // Return a string of the number here!
+    let str = num.toString()+''
+    return   str;
+  }
+
+  console.log(numberToString(123))
+  console.log(numberToString(999))
+  console.log(numberToString(-100))
