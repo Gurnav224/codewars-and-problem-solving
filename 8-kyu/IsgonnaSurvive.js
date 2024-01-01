f2c2d064d24a910405d5dcc87d42b48c4063d36e
Gurnav224 <chaudharyg856@gmail.com>
2024-01-01 14:59:36 +0530

    8kyu isgonnasurive

---
+++ 8-kyu/IsgonnaSurvive.js
@@ -0,0 +1,13 @@
+function hero(bullets, dragons){
+    //Get Coding!
+
+    if(bullets>=2*dragons){
+        return true
+    }
+    return false
+      
+    }
+
+    
+
+    console.log(hero(10,5))
