26775ea51ab044decbed15515f63f8117f2ec6a7
Gurnav224 <chaudharyg856@gmail.com>
2024-01-22 21:34:37 +0530

    solve firstIndex

---
+++ problems-solving/firstIndex.js
@@ -0,0 +1,12 @@
+
+
+let arr = ["hello","word","flow","house"]
+
+let firstIndex = arr[0]
+
+let lastIndex = arr[arr.length-1]
+
+
+console.log("firstIndex: ",firstIndex);
+
+console.log("lastIndex: ",lastIndex)
