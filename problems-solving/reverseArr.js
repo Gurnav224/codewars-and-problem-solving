1e675dbafba543258d540b782487028721868b92
Gurnav224 <chaudharyg856@gmail.com>
2024-01-22 21:38:34 +0530

    solve reverse Array

---
+++ problems-solving/reverseArr.js
@@ -0,0 +1,17 @@
+
+
+
+function revArr(arr){
+    let newarr = []
+    for(let i=arr.length; i>0; i--){
+      newarr.push(i)
+    }
+    return newarr
+}
+
+
+
+
+let arr=[1,2,3,4,5];
+
+console.log(revArr(arr))
