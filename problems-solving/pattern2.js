eef13e42d02d672ffcbfe717eb496d67979e9a75
Gurnav224 <chaudharyg856@gmail.com>
2024-01-22 21:37:31 +0530

    solve pattern2

---
+++ problems-solving/pattern2.js
@@ -0,0 +1,17 @@
+
+
+function printSquare(n){
+   
+
+    for(let i=0; i<n; i++){
+        let str = ""
+        for(let j=0; j<i; j++){
+            str += "#"
+        }
+    console.log(str)
+
+    }
+
+}
+
+printSquare(6)
