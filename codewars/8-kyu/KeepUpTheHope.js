9247c1156af99e82067cb38ec930be33e97c44f6
Gurnav224 <chaudharyg856@gmail.com>
2024-01-23 11:13:18 +0530

    solve 8-kyu keepupthehope

---
+++ codewars/8-kyu/KeepUpTheHope.js
@@ -0,0 +1,13 @@
+
+function hoopCount(n){
+    // if(n>=10){
+    //     return "Great, now move on to tricks"
+    // }
+    // else{
+    //     return "Keep at it until you get it"
+    // }
+
+    return  n>=10 ? "Great, now move on to tricks":"keep at it until you get it"
+}
+
+console.log(hoopCount(3))
