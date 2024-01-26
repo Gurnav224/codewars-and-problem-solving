1b119ad82a6715f17af6ddfab0a3995cccafa7b7
Gurnav224 <chaudharyg856@gmail.com>
2024-01-26 12:47:39 +0530

    solve 8kyu Isthis my tail

---
+++ codewars/8-kyu/IsThismyTail.js
@@ -0,0 +1,12 @@
+
+function correctTail(bod, tail){
+ 
+return    bod.split("")[bod.length-1]=== tail
+   
+    
+}
+  
+
+
+console.log(correctTail("Fox","x"))
+console.log(correctTail("Dbtfwwphkqqsamnojmekyr", "c"))
