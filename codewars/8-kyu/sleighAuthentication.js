fb5e2bf8f823d6baeeb117c1b588b4ec64f67c26
Gurnav224 <chaudharyg856@gmail.com>
2024-02-01 15:38:04 +0530

    solve 8-kyu sleighAuthentication

---
+++ codewars/8-kyu/sleighAuthentication.js
@@ -0,0 +1,14 @@
+
+
+function authentication(name,password){
+    if(name.toLowerCase()==='santa claus',password.toLowerCase()==='ho ho ho!'){
+        return true
+    }
+    else{
+        return false
+
+    }
+}
+
+
+console.log(authentication("Santa Claus", "Ho Ho Ho!"))
