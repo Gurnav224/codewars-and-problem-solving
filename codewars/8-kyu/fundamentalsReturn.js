4e43c3a6f78982bfe89fb0e0a3e493aaeef82305
Gurnav224 <chaudharyg856@gmail.com>
2024-02-03 11:19:26 +0530

    solve 8-kyu fundamental Returns

---
+++ codewars/8-kyu/fundamentalsReturn.js
@@ -0,0 +1,34 @@
+
+
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+
+function mulitply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function mod(a,b){
+    return a%b
+}
+
+function exponent(a,b){
+    return a**b
+}
+
+
+console.log(add(1,2))
+console.log(subtract(5,8))
+console.log(mulitply(3,5))
+console.log(divide(9,2))
+console.log(mod(7,6))
+console.log(exponent(2,2))
