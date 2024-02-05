45d6673d46f3217213ffb92eb2ab87394a366c4e
Gurnav224 <chaudharyg856@gmail.com>
2024-02-05 18:07:01 +0530

    solve 8-kyu remove Exclamaination

---
+++ codewars/8-kyu/removeExclamation.js
@@ -0,0 +1,11 @@
+
+
+function removeExclamination(str,num){
+  for(let i=0;i<num; i++ ){
+    str = str.replace("!","")
+  }
+  return str.replace()
+}
+
+
+console.log(removeExclamination("hie!",2))
