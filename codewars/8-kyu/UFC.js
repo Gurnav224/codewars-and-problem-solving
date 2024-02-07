841847b5cf9ad12a8674c6d5e0fbc0e26a0f26a7
Gurnav224 <chaudharyg856@gmail.com>
2024-02-07 20:44:17 +0530

    solve 8-kyu UFC fighter

---
+++ codewars/8-kyu/UFC.js
@@ -0,0 +1,14 @@
+
+
+function fighterWinner(fighter){
+  const fighterName = fighter.toLowerCase();
+  if(fighterName==="george saint pierre"){
+return "I am not impressed by your performance"
+  }
+  if(fighterName==="conor mcgregor"){
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+}
+
+console.log(fighterWinner("George Saint Pierre"))
+console.log(fighterWinner('Conor McGregor'))
