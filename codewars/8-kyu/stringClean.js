c1570433708aefb05247cc6e51067be9789d1098
Gurnav224 <chaudharyg856@gmail.com>
2024-02-15 19:09:43 +0530

    solve 8-kyu string Clean

---
+++ codewars/8-kyu/stringClean.js
@@ -0,0 +1,11 @@
+
+
+function stringClean(s){
+    return s.replace(/[0-9]/g,'')
+}
+
+console.log(stringClean("! !"))
+
+console.log(stringClean("123456436"))
+
+console.log(stringClean("This looks5 grea8t!"))
