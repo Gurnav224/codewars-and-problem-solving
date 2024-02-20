9d249b6891f25588267fa01957cc90a4b20cc0bd
Gurnav224 <chaudharyg856@gmail.com>
2024-02-20 10:43:43 +0530

    solve 8-kyu sixToast

---
+++ codewars/8-kyu/sixToast.js
@@ -0,0 +1,13 @@
+
+
+function sixToast(num){
+    let six = 6;
+     let res = num-six
+ return Math.abs(res)
+  
+}
+
+
+console.log(sixToast(5))
+console.log(sixToast(3))
+console.log(sixToast(17))
