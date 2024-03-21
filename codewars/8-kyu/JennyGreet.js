7cd3875b2f1046af692504b37f76868842f2fe53
Gurnav224 <chaudharyg856@gmail.com>
2024-03-21 11:05:26 +0545

    solve 8-kyu JennyGreet

---
+++ codewars/8-kyu/JennyGreet.js
@@ -0,0 +1,11 @@
+
+
+function greet(name){
+    if(name==="Johnny"){
+        return "Hello, my love!"
+    }
+    return "Hello, "+name+"!"
+}
+
+console.log(greet("Jim"));
+console.log(greet("Johnny"))
