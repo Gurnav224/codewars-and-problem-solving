0bd9973662a8543573ddd586b695241a25f3b57b
Gurnav224 <chaudharyg856@gmail.com>
2024-03-24 11:27:43 +0545

    solve 8-kyu quarterOf

---
+++ codewars/8-kyu/quarterOf.js
@@ -0,0 +1,18 @@
+
+
+function quarterOf(month){
+    if(month>=1&&month<=3){
+        return 1;
+    }
+    else if(month>=4&&month<=6){
+        return 2;
+    }
+    else if(month>=7&&month<=9){
+        return 3;
+    }
+    else return 4;
+}
+
+console.log(quarterOf(3))
+console.log(quarterOf(8))
+console.log(quarterOf(11))
