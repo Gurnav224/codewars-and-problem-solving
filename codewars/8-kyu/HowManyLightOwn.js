64fd947b17338ff21f3df92b94fc77b22fa150e6
Gurnav224 <chaudharyg856@gmail.com>
2024-04-04 20:06:40 +0545

    solve 8-kyu HowManyLightOwn

---
+++ codewars/8-kyu/HowManyLightOwn.js
@@ -0,0 +1,11 @@
+
+function howManyLightsabersDoYouOwn(name) {
+    if(name==='Zach'){
+        return 18
+    }
+    return 0
+  }
+
+  console.log(howManyLightsabersDoYouOwn("Adam"))
+  console.log(howManyLightsabersDoYouOwn("Zach"))
+  console.log(howManyLightsabersDoYouOwn())
