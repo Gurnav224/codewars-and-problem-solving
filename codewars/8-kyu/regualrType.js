af10096d87de0c4bb9cf5ef5adfcb9b03f72877d
Gurnav224 <chaudharyg856@gmail.com>
2024-04-16 14:23:38 +0545

    solve 8-kyu regualrType

---
+++ codewars/8-kyu/regualrType.js
@@ -0,0 +1,15 @@
+
+
+class Ball{
+    constructor(ballType="regular"){
+        this.ballType = ballType
+    }
+
+    
+}
+
+
+let ball1 = new Ball()
+let ball2 = new Ball("fit phase")
+
+console.log(ball1.ballType)
