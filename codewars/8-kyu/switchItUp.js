7bcdec42f4593c57f71705536b3d4c74ad2db819
Gurnav224 <chaudharyg856@gmail.com>
2024-04-20 11:07:24 +0545

    solve 8-kyu switchItup

---
+++ codewars/8-kyu/switchItUp.js
@@ -0,0 +1,41 @@
+
+
+
+function switchitUp(number){
+    switch (number) {
+        case 0:
+            return "zero"
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case 4:
+            return "Four"
+        case 5:
+            return "Five"
+        case 6:
+            return "Six"
+        case 7:
+            return "Seven"
+        case 8:
+            return "Eight"
+        case 9:
+            return "Nine"
+        default:
+            break;
+    }
+}
+
+
+console.log(switchitUp(0))
+console.log(switchitUp(1))
+console.log(switchitUp(2))
+console.log(switchitUp(3))
+console.log(switchitUp(4))
+console.log(switchitUp(5))
+console.log(switchitUp(6))
+console.log(switchitUp(7))
+console.log(switchitUp(8))
+console.log(switchitUp(9))
